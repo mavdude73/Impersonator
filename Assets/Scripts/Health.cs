30b1fb233634bc345fa6d3d9eb1b4e60693382ea
FelixMeow <fm_matrix@hotmail.com>
2016-09-14 21:52:36 +0100

    networking

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+	[SyncVar (hook = "OnChangeHealth")]public int currentHealth = maxHealth;
+	public RectTransform healthBar;
+
+	public void TakeDamage (int amount)
+	{
+		if(!isServer)
+		{
+			return;
+		}
+
+		currentHealth -= amount;
+		if(currentHealth <= 0)
+		{
+			currentHealth = maxHealth;
+			RpcRespawn();
+		}
+
+
+	}
+
+	void OnChangeHealth (int health)
+	{
+		healthBar.sizeDelta = new Vector2(health * 2, healthBar.sizeDelta.y);
+	}
+
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if(isLocalPlayer)
+		{
+			transform.position = Vector3.zero;
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	void Update ()
+	{
+		if(!isLocalPlayer)
+		{
+			return;
+		}
+	
+		float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0,x,0);
+		transform.Translate(0,0,z);
+
+		if(Input.GetButtonDown("Fire1"))
+		{
+			CmdFire();
+		}
+	
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.green;
+
+	}
+
+	[Command]
+	void CmdFire()
+	{
+		GameObject bullet = (GameObject) Instantiate(bulletPrefab, bulletSpawn.position,bulletSpawn.rotation);
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6.0f;
+
+		NetworkServer.Spawn(bullet);
+
+		Destroy(bullet, 2.0f);
+	}
+
+}
